f34df28b8e3d634b30754197b47c2f0d572829f3
namu254 <vignenamu@gmail.com>
2019-07-04 17:47:47 +0300

    make it better

--- assignment_sys/settings.py
+++ assignment_sys/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -160,3 +161,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 LOGIN_REDIRECT_URL='/'
 
 USE_L10N = False
+# Activate Django-Heroku.
+django_heroku.settings(locals())
